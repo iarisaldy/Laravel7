6283b99a1bd9188569cbe30dd7177f12bd5f7e15
unknown <iarisaldy@gmail.com>
2020-06-24 15:58:29 +0700

    menambahkan view dan routes

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                WEB PROFILE
+                ADMIN WEB PROFILE
             </div>
 
             <div class="links">
